019990c634e1a9f66360d7a049ebbfd285ab8124
arunrajrat <arunrajrat@gmail.com>
2024-03-18 11:07:25 +0000

    added module-1 changes

---
+++ gitcommands/src/main/java/org/automation/gitcommands/FirstClass.java
@@ -0,0 +1,16 @@
+package org.automation.gitcommands;
+
+public class FirstClass {
+	
+	
+	public void Method1() {
+		System.out.println("Method1");
+	}
+	
+	public void Module1() {
+		System.out.println("module 1 method");
+		System.out.println("module 1 method");
+		
+	}
+
+}

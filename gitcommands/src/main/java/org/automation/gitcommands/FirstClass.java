c302efe174930ebf3c63393dc161b16af27b1120
arunrajrat <arunrajrat@gmail.com>
2024-03-18 11:24:49 +0000

    made changes in firstclass

--- gitcommands/src/main/java/org/automation/gitcommands/FirstClass.java
+++ gitcommands/src/main/java/org/automation/gitcommands/FirstClass.java
@@ -7,7 +7,12 @@ public class FirstClass {
 		System.out.println("Method1");
 	}
 	
-	public void Module1() {
+	public void Module1(String ad) {
+		System.out.println("module 1 method");
+		System.out.println("module 1 method");
+		
+	}
+	public void Module1(String a, String b) {
 		System.out.println("module 1 method");
 		System.out.println("module 1 method");
 		
